85d1c0bbf1fe1d8cad6c6446b44bce1b44ee8285
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2024-10-13 14:31:34 +0300

    Initial Project setup

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using HRManagementApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRManagementApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Register> Registers { get; set; }
+    }
+}
---
+++ Models/Register.cs
@@ -0,0 +1,12 @@
+﻿namespace HRManagementApp.Models
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        public required string Surname { get; set; }
+        public required string OtherNames { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string? Photo {  get; set; }
+        public required string StaffAccessCode { get; set; }
+    }
+}
